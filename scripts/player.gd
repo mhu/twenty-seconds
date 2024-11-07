7ab416fdb85cf5888df236e9cdd6b81fd70cefce
mhu <marcel.huismann@protonmail.ch>
2024-11-07 01:43:53 -0500

    feat: add enemy and get distance to closest enemy

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
+@onready var enemies = $"../Enemies"
+
 @export var speed = 500
+@export var attack_range = 200
 
 
 func _ready() -> void:
@@ -17,3 +20,22 @@ func _process(_delta: float) -> void:
 
     velocity = direction * speed
     move_and_slide()
+
+    var enemy = get_closest_enemy_in_range()
+    if enemy == null:
+        return
+
+
+func get_closest_enemy_in_range():
+    if enemies == null:
+        return
+
+    var closest_enemy = null
+    var closest_enemy_range = INF
+
+    for enemy in enemies.get_children():
+        if position.distance_to(enemy.position) < attack_range and position.distance_to(enemy.position) < closest_enemy_range:
+            closest_enemy = enemy
+            closest_enemy_range = position.distance_to(enemy.position)
+
+    return closest_enemy
