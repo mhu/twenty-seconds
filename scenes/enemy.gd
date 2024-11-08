f4d870bf21089a96379b610f23390f500af82229
mhu <marcel.huismann@protonmail.ch>
2024-11-08 14:22:45 -0500

    feat: spawn enemies and make enemies follow the player

---
+++ scenes/enemy.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var player: CharacterBody2D = get_node("/root/Main/Player")
+var speed = 100.0
+
+
+func _ready() -> void:
+    pass
+
+
+func _process(delta: float) -> void:
+    handle_movement(delta)
+
+
+func handle_movement(delta: float) -> void:
+    var direction = (player.global_position - global_position).normalized()
+    move_and_collide(direction * speed * delta)
---
+++ scenes/main.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@onready var loaded_enemy: Resource
+@onready var player := $Player
+@onready var enemies := $Enemies
+var spawn_distance_to_player: float = 300.0
+
+
+func _ready() -> void:
+    var timer = Timer.new()
+    add_child(timer)
+    timer.wait_time = 1.0
+    timer.timeout.connect(_spawn_enemy)
+    timer.start()
+
+    loaded_enemy = preload("res://scenes/enemy.tscn")
+
+
+func _process(_delta: float) -> void:
+    pass
+
+
+func _spawn_enemy() -> void:
+    var enemy = loaded_enemy.instantiate()
+    var enemy_position_x = [
+        player.position.x - spawn_distance_to_player,
+        player.position.x + spawn_distance_to_player,
+    ]
+    var enemy_position_y = [
+        player.position.y - spawn_distance_to_player,
+        player.position.y + spawn_distance_to_player,
+    ]
+    enemy.position = Vector2(
+        enemy_position_x.pick_random(),
+        enemy_position_y.pick_random(),
+    )
+    enemies.add_child(enemy)
