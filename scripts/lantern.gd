1b961e0642f9a49534d54cd3762b56660404ad3d
mhu <marcel.huismann@protonmail.ch>
2024-11-08 14:01:57 -0500

    feat: add simple background

--- scripts/lantern.gd
+++ scripts/lantern.gd
@@ -3,5 +3,5 @@ extends PointLight2D
 
 func _ready() -> void:
     var tween = get_tree().create_tween().set_loops()
-    tween.tween_property(self, "scale", Vector2(3.0, 3.0), 3.0)
-    tween.tween_property(self, "scale", Vector2(4.0, 4.0), 3.0)
+    tween.tween_property(self, "scale", Vector2(3.5, 3.5), 2.0)
+    tween.tween_property(self, "scale", Vector2(4.0, 4.0), 2.0)
